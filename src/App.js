b557b1af06cd1ca1fb77a4807049c5f0bb7e1b24
ALDAR-G7 <ronnyald007@gmail.com>
2022-09-26 12:59:09 -0400

    Componentes terminados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import datosTestimonios from './datos/datosTestimonios';
 import Testimonio from './componentes/Testimonio';
 
 function App() {
@@ -6,7 +7,33 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio/>
+        <Testimonio
+          nombre={datosTestimonios.persona3.nombre}
+          pais={datosTestimonios.persona3.pais}
+          imagen={datosTestimonios.persona3.imagen}
+          alt={datosTestimonios.persona3.alt}
+          cargo={datosTestimonios.persona3.cargo}
+          empresa={datosTestimonios.persona3.empresa}
+          testimonio={datosTestimonios.persona3.testimonio} 
+        />
+        <Testimonio
+          nombre={datosTestimonios.persona1.nombre}
+          pais={datosTestimonios.persona1.pais}
+          imagen={datosTestimonios.persona1.imagen}
+          alt={datosTestimonios.persona1.alt}
+          cargo={datosTestimonios.persona1.cargo}
+          empresa={datosTestimonios.persona1.empresa}
+          testimonio={datosTestimonios.persona1.testimonio} 
+        />
+        <Testimonio
+          nombre={datosTestimonios.persona2.nombre}
+          pais={datosTestimonios.persona2.pais}
+          imagen={datosTestimonios.persona2.imagen}
+          alt={datosTestimonios.persona2.alt}
+          cargo={datosTestimonios.persona2.cargo}
+          empresa={datosTestimonios.persona2.empresa}
+          testimonio={datosTestimonios.persona2.testimonio} 
+        />
       </div>
     </div>
   );
---
+++ src/datos/datosTestimonios.js
@@ -0,0 +1,31 @@
+const datosTestimonios={
+    persona3:{
+        nombre:'Emma Bostian',
+        pais:'Suecia',
+        imagen:'imagen3',
+        alt: 3,
+        cargo:'Ingeniera de Software',
+        empresa:'Spotify',
+        testimonio:'Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.',
+    },
+    persona1:{
+        nombre:'Shawn Wang',
+        pais:'Singapur',
+        imagen:'imagen1',
+        alt: 1,
+        cargo:'Ingeniero de Software',
+        empresa:'Amazon',
+        testimonio:'Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.',
+    },
+    persona2:{
+        nombre:'Sarah Chima',
+        pais:'Nigeria',
+        imagen:'imagen2',
+        alt: 2,
+        cargo:'Ingeniera de Software',
+        empresa:'ChatDesk',
+        testimonio:'freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.',
+    },
+};
+
+export default datosTestimonios;
